016e1a1c536999a6e184bffe24b580852bab1cbc
Avergrove <Avergrove@gmail.com>
2019-01-17 18:11:17 +0800

    Project 2 Implemented

---
+++ 2-SumOfEvenFibs/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace _2_SumOfEvenFibs
+{
+  class Program
+  {
+    /**
+     * Analysis:
+     *  Note 1:
+     *    - Fibs always sum the previous two numbers, whether the outcome is odd or even is also based on the aforementioned two numbers.
+     *    - Even + even = even;
+     *    - Even + odd = odd;
+     *    - Odd + odd = even;
+     *    
+     *  Note 2:
+     *    - Fibs start with 1, 1, which is odd-odd, so we can get the pattern:
+     *      - odd, odd, (even , odd, odd), (even, odd, odd), (even ..
+     *      - Even occurs on N = 3, 6, 9, 12..
+     *      
+     *  Note 3:
+     *    - Recursive programming may be a bit slow, please bring out Dynamic Programming you learned in undergrad!
+     *    - Have fun tabulating! \o/
+     */
+
+    private static long[] fibTable;
+
+    static void Main(String[] args)
+    {
+      long t = long.Parse(Console.ReadLine());
+      for (long a0 = 0; a0 < t; a0++)
+      {
+        long upperLimit = long.Parse(Console.ReadLine());
+        fibTable = new long[100000];
+
+        long evenSum = 0;
+        long iteration = 0;
+        long fibN = 0;
+
+        while(true) { 
+          fibN = GetFib(iteration);
+
+          if(fibN > upperLimit)
+          {
+            break;
+          }
+
+          if(iteration % 3 == 2) // We need to consider that arrays start from 0, hence 2 instead of 0.
+          {
+            evenSum += fibN;
+          }
+
+          iteration++;
+        }
+        Console.WriteLine(evenSum);
+
+      }
+    }
+
+    static long GetFib(long n)
+    {
+      if(n == 0)
+      {
+        fibTable[n] = 1;
+      }
+
+      else if(n == 1)
+      {
+        fibTable[n] = 1;
+      }
+
+      else
+      {
+        if (fibTable[n] == 0)
+        {
+          fibTable[n] = fibTable[n - 1] + fibTable[n - 2];
+        }
+      }
+
+      return fibTable[n];
+
+    }
+  }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -4,6 +4,24 @@ namespace ProjectEuler
 {
   class Program
   {
+    /**
+     * The problem:
+     * Find the sum of numbers which are multiples of 3 and 5, from [0,N).
+     */ 
+
+    /**
+     * Explanation:
+     * Note 1:
+     *  Multiples of 3 and 5 form an Arithmetric Progression.
+     *  sum3 = 3(1+2+3+4..)
+     *  sum5 = 5(1+2+3+4..)
+     * 
+     *  We can exploit this to achieve linear time.
+     * 
+     * Note 2:
+     *  sum15 needs to be deducted since it is added twice during the summing of sum3 with sum15.
+     */
+
     static void Main(String[] args)
     {
       int t = Convert.ToInt32(Console.ReadLine());
@@ -12,19 +30,6 @@ namespace ProjectEuler
         long n = Int64.Parse(Console.ReadLine());
         long upperLimit = n - 1; // Since these numbers must be below N
 
-        /**
-         * Explanation:
-         * Note 1:
-         *  Multiples of 3 and 5 form an Arithmetric Progression.
-         *  sum3 = 3(1+2+3+4..)
-         *  sum5 = 5(1+2+3+4..)
-         * 
-         *  We can exploit this to achieve lienar time.
-         * 
-         * Note 2:
-         *  sum15 needs to be deducted since it is added twice during the summing of sum3 with sum15.
-         * 
-         */ 
 
         long sum = (3 * Summation(upperLimit / 3)) + (5 * Summation(upperLimit / 5)) - (15 * Summation(upperLimit / 15));
 
